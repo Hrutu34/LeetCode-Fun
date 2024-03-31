7ddc2e9ddf610f917887c4365075dca6d19983d2
Hrutu34 <survehrutu@gmail.com>
2024-03-31 13:50:48 +0530

    updated subsets solution

--- Subsets/subsets.py
+++ Subsets/subsets.py
@@ -1,16 +1,14 @@
 #Amrit BKL
-def subsets_bana_bc(nums): #[1,2,3]
-    def tu_phir_aagaya(start, path):
-        print("Index Value before for:",start)
+def subsets(nums): 
+    def backtrack(start, path):
         final.append(path)
         for i in range(start, len(nums)):
-            print("Index Value after for:",i)
-            tu_phir_aagaya(i + 1, path + [nums[i]])
+            backtrack(i + 1, path + [nums[i]])
 
     final = []
-    tu_phir_aagaya(0, [])
+    backtrack(0, [])
     return final
 
-Test_Case_ki_MKC = [2, 5, 8, 4]
-print(subsets_bana_bc(Test_Case_ki_MKC))
+num_1 = [2, 5, 8, 4]
+print(subsets(num_1))
 
